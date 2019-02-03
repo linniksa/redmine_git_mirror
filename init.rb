21e1d78b912caddc8dbe445b3eed938f4b4b3572
Sergey Linnik <linniksa@gmail.com>
2019-02-03 21:28:09 +0300

    v0.6

--- init.rb
+++ init.rb
@@ -6,7 +6,7 @@ Redmine::Plugin.register :redmine_git_mirror do
   name 'Git Mirror'
   author 'Sergey Linnik'
   description 'Add ability to create readonly mirror of remote git repository'
-  version '0.5.0'
+  version '0.6.0'
   url 'https://github.com/linniksa/redmine_git_mirror'
   author_url 'https://github.com/linniksa'
 
