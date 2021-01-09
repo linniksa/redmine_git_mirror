d6a3b751bfd4d67cd4430790a79f685f199bd2e4
Florin Hillebrand <florin.hillebrand@tuwien.ac.at>
2021-01-10 00:02:28 +0100

    Add support for gitea webhook.
    Reuse the existing github webhook and generalize it, so
    that it can be used with gitea too.
    
    Signed-off-by: Florin Hillebrand <florin.hillebrand@tuwien.ac.at>

--- config/routes.rb
+++ config/routes.rb
@@ -2,3 +2,4 @@
 match 'sys/git_mirror/fetch', to: 'git_mirror#fetch', via: [:get, :post]
 match 'sys/git_mirror/gitlab', to: 'git_mirror#gitlab', via: [:post]
 match 'sys/git_mirror/github', to: 'git_mirror#github', via: [:post]
+match 'sys/git_mirror/gitea', to: 'git_mirror#gitea', via: [:post]
