31631dfd87c7466cd01711981cfb2ae0b8776030
Anthony Georges <anthony.georges@team.telstra.com>
2022-11-23 22:24:50 +1100

    Add support for Report Last Commit

--- lib/redmine_git_mirror/patches/repositories_helper_patch.rb
+++ lib/redmine_git_mirror/patches/repositories_helper_patch.rb
@@ -10,7 +10,11 @@ module RedmineGitMirror
           :required => true,
           :disabled => !repository.safe_attribute?('url'),
           ) +
-          content_tag('em', l(:text_git_mirror_url_note), :class => 'info')
+          content_tag('em', l(:text_git_mirror_url_note), :class => 'info')) +
+          content_tag('p', form.check_box(
+                        :report_last_commit,
+                        :label => l(:label_git_report_last_commit)
+                         )
         )
       end
     end
